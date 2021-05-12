bbf52afaa352523f3a7a61aaa541e7b85f8f6181
Wade Hammes <wadehammes@users.noreply.github.com>
2021-05-12 15:54:14 -0400

    ✨ add title and description (#7)

--- src/components/PageStructure/PageStructure.component.tsx
+++ src/components/PageStructure/PageStructure.component.tsx
@@ -1,12 +1,33 @@
 import React, { FC } from "react";
+import Head from "next/head";
 import { Content, Grid } from "src/components/Layout";
 import Sidebar from "src/components/Sidebar/Sidebar.component";
+import { constants } from "src/constants";
 
-export const PageStructure: FC = ({ children }) => (
-  <Grid data-testid="scPageStructure" gridHeight="100vh">
-    <Sidebar />
-    <Content data-testid="scContent">{children}</Content>
-  </Grid>
+interface PageStructureProps {
+  meta?: {
+    title: string;
+    description: string;
+  };
+}
+
+export const PageStructure: FC<PageStructureProps> = ({
+  children,
+  meta = {
+    title: constants.title,
+    description: constants.description,
+  },
+}) => (
+  <>
+    <Head>
+      <title>{meta.title}</title>
+      <meta name="description" content={meta.description} />
+    </Head>
+    <Grid data-testid="scPageStructure" gridHeight="100vh">
+      <Sidebar />
+      <Content data-testid="scContent">{children}</Content>
+    </Grid>
+  </>
 );
 
 export default PageStructure;
---
+++ src/constants.ts
@@ -0,0 +1,5 @@
+export const constants = {
+  title: "Sunset & Chill",
+  description:
+    "Sunset & Chill is a semi-regular community dance and arts event based in Washington D.C. showcasing fire and flow artists, music, and vendors.",
+};
