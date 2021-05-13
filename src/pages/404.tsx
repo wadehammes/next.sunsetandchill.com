d15ff25c90695d253386a8d309746f6766fceca7
Wade Hammes <wade.hammes@gmail.com>
2021-05-13 07:41:44 -0400

    add props to 404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,5 +1,12 @@
+import { GetStaticProps } from "next";
 import { FC } from "react";
 
 const e404: FC = () => <div>404!</div>;
+
+// eslint-disable-next-line require-await
+export const getStaticProps: GetStaticProps = async () => ({
+  props: {},
+  revalidate: 1,
+});
 
 export default e404;
