1348a0000a2b3b98cd3a9aa8dc75dc720ea85637
Wade Hammes <wadehammes@users.noreply.github.com>
2021-11-12 08:26:47 -0500

    ⬆️ update packages (#175)

--- jest.config.ts
+++ jest.config.ts
@@ -3,15 +3,19 @@ import type { Config } from "@jest/types";
 
 // Sync object
 const config: Config.InitialOptions = {
+  preset: "ts-jest",
   verbose: true,
   testPathIgnorePatterns: ["<rootDir>/.next/", "<rootDir>/node_modules/"],
   setupFiles: ["<rootDir>/.jest/setEnvVars.ts"],
   setupFilesAfterEnv: ["<rootDir>/.jest/setupTests.ts"],
   transform: {
     "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest",
-    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
   },
   moduleDirectories: ["node_modules", "<rootDir>"],
+  testEnvironment: "jsdom",
+  moduleNameMapper: {
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+  },
 };
 
 // eslint-disable-next-line import/no-default-export
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,8 @@
 import App from "next/app";
-import React from "react";
 import { ThemeProvider } from "styled-components";
 import { GlobalStyles } from "src/styles/global";
 import { theme } from "src/styles/theme";
+import Script from "next/script";
 
 export default class WadeHammes extends App {
   render() {
@@ -14,6 +14,15 @@ export default class WadeHammes extends App {
         <ThemeProvider theme={theme}>
           <Component {...pageProps} />
         </ThemeProvider>
+        <Script
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=G-7THQXZKB24"
+        />
+        <Script id="gtag" strategy="afterInteractive">
+          {
+            "window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date()); gtag('config', 'G-7THQXZKB24');"
+          }
+        </Script>
       </>
     );
   }
